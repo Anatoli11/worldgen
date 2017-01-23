21c477d5cef31f4906776d828fc2559eef88b61b
Tony Tkach <ttkach@outlook.com>
2017-01-23 06:53:49 -0600

    Add files via upload

---
+++ Map/Static/stMapMineralProfileList SQL test.sql
@@ -0,0 +1,191 @@
+USE [WorldGen]
+GO
+
+/****** Object:  Table [dbo].[stMapMineralProfileList]    Script Date: 2016-02-08 9:53:22 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[stMapMineralProfileList](
+	[idMapMineralProfileList] [int] NOT NULL,
+	[strMapMineralProfileName] [nvarchar](50) NOT NULL,
+	[intMapMineralProfileInitialValue] [int] NULL,
+	[strMapMineralProfileNotes] [nchar](500) NULL
+) ON [PRIMARY]
+
+GO
+
+
+insert into stMapMineralProfileList values
+(1,'Adamantine spar',1,'variety of corundum'),
+(2,'Agate',1,'variety of quartz'),
+(3,'Alabaster',1,'variety of gypsum'),
+(4,'Alexandrite',1,'variety of chrysoberyl'),
+(5,'Allingite',1,'synonym of amber'),
+(6,'Alum',1,''),
+(7,'Amazonite',1,'variety of microcline'),
+(8,'Amber',1,'fossilized resin'),
+(9,'Amethyst',50,'purple variety of quartz'),
+(10,'Ammolite',1,'organic; also a gemstone'),
+(11,'Amosite',1,'asbestiform grunerite'),
+(12,'Antozonite',1,'variety of fluorite'),
+(13,'Anyolite',1,'metamorphic rock - zoisite, ruby, and hornblende'),
+(14,'Aquamarine',1,'light blue variety of beryl'),
+(15,'Argentite',8,'high temperature form of acanthite'),
+(16,'Asbestos',1,'fibrous serpentine- or amphibole minerals'),
+(17,'Auerlite',1,'variety of thorite'),
+(18,'Avalite',1,'chromian variety of illite'),
+(19,'Aventurine',2,'variety of quartz'),
+(20,'Abelsonite',1,''),
+(21,'Abenakiite',1,'Ce'),
+(22,'Abernathyite',1,''),
+(23,'Abhurite',1,''),
+(24,'Abramovite',1,''),
+(25,'Abswurmbachite',1,''),
+(26,'Acanthite',1,''),
+(27,'Achavalite',1,''),
+(28,'Actinolite',4,''),
+(29,'Acuminite',1,''),
+(30,'Adamite',1,''),
+(31,'Adamsite',45,'Y'),
+(32,'Adelite',1,''),
+(33,'Admontite',1,''),
+(34,'Aegirine',1,''),
+(35,'Aenigmatite',6,''),
+(36,'Aerinite',1,''),
+(37,'Aerugite',1,''),
+(38,'Aeschynite',1,'Ce')
+
+
+
+
+Afghanite
+Afwillite
+Agardite
+Agrellite
+Agrinierite
+Aguilarite
+Aheylite
+Ahlfeldite
+Aikinite
+Ajoite
+Akaganéite
+Akatoreite
+Akdalaite
+Åkermanite
+Akhtenskite
+Akimotoite
+Akrochordite
+Aksaite
+Aktashite
+Alabandite
+Alacránite
+Alamosite
+Alarsite
+Albite
+Albrechtschraufite
+Aldermanite
+Aleksite
+Alforsite
+Algodonite
+Aliettite
+Allabogdanite
+Allactite
+Allanite
+Allanpringite
+Allargentum
+Alleghanyite
+Alloclasite
+Allophane
+Alluaivite
+Alluaudite
+Almandine
+Almarudite
+Alsakharovite-Zn
+Alstonite
+Altaite
+Althausite
+Althupite
+Altisite
+Alum-(K)
+Aluminite
+Aluminium
+Alunite
+Alunogen
+Amakinite
+Amarantite
+Amblygonite
+Ameghinite
+Amesite
+Amicite
+Amphibole (mineral group)
+Analcime (analcite)
+Anandite
+Anapaite
+Anatase
+Ancylite (mineral group)
+Andalusite
+Andersonite
+Andesine
+Andorite (IV/VI)
+Andradite
+Andyrobertsite
+Anglesite
+Anhydrite
+Ankerite
+Annabergite
+Annite
+Anorthite
+Anorthoclase
+Antarcticite
+Anthonyite
+Anthophyllite
+Antigorite
+Antimony
+Antitaenite
+Antlerite
+Apachite
+Apatite (mineral group)
+Aphthitalite
+Apophyllite
+Aragonite
+Arcanite
+Archerite
+Arctite
+Arcubisite
+Ardaite
+Arfvedsonite
+Argentite
+Argutite
+Argyrodite
+Armalcolite
+Arsenic
+Arseniosiderite
+Arsenoclasite
+Arsenolite
+Arsenopyrite
+Arthurite
+Artinite
+Artroeite
+Ashburtonite
+Ashoverite
+Asisite
+Astrophyllite
+Atacamite
+Athabascaite
+Atheneite
+Aubertite
+Augelite
+Augite
+Aurichalcite
+Auricupride
+Aurostibite
+Austinite
+Autunite
+Avicennite
+Avogadrite
+Awaruite
+Axinite (mineral group)
+Azurite
