50868a6d87ec2dffdd533ed5eb69a46c967da792
Tony Tkach <ttkach@outlook.com>
2017-03-25 09:13:00 -0600

    File-add
    Separated out the JavaScript files for my two generation sandbox areas.

---
+++ Generation/MapGen/mapgen.js
@@ -0,0 +1,2 @@
+// This is a placeholder file for the mapgen JavaScript code
+alert('hi from mapgen.js');
---
+++ Generation/PeopleGen/peoplegen.js
@@ -0,0 +1,2 @@
+// This is a placeholder file for the peoplegen JavaScript code
+alert('hi from peoplegen.js');
